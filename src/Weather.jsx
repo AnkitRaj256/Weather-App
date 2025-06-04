e4532435500acccdd217e9473e7e239bb4a63de8
Ankit Raj <163820076+AnkitRaj256@users.noreply.github.com>
2025-06-04 21:49:20 +0530

    Update Weather.jsx

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -4,7 +4,7 @@ import { CardContent, Cards } from './components/Cards'
 import Button from './components/Button'
 import { Sun, Cloud, CloudRain, Snowflake, Zap } from 'lucide-react'
 
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 const Weather = () => {
   const [city, setCity] = useState('')
